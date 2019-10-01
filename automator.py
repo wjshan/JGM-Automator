8660f24d21cfba72a645a7c2d92898619524ec15
单炜佳 <shanweijia@jiaaocap.com>
2019-10-01 11:04:28 +0800

    fix(): 修改自动火车原理， 并将自动划金币合并

--- automator.py
+++ automator.py
@@ -1,6 +1,8 @@
 from target import TargetType
 from cv import UIMatcher
 import uiautomator2 as u2
+import time
+import random
 
 
 class Automator:
@@ -17,15 +19,14 @@ class Automator:
         """
         while True:
             # 判断是否出现货物。
-            for target in TargetType:
-                self._match_target(target)
+            self._match_target()
 
             # 简单粗暴的方式，处理 “XX之光” 的荣誉显示。
             # 当然，也可以使用图像探测的模式。
-            self.d.click(550, 1650)
+            self.d.click(439, 1092)
 
             # 滑动屏幕，收割金币。
-            self._swipe()
+            # self._swipe()
 
     def _swipe(self):
         """
@@ -43,15 +44,15 @@ class Automator:
         获取指定建筑的屏幕位置。
         """
         positions = {
-            1: (294, 1184),
-            2: (551, 1061),
-            3: (807, 961),
-            4: (275, 935),
-            5: (535, 810),
-            6: (799, 687),
-            7: (304, 681),
-            8: (541, 568),
-            9: (787, 447)
+            1: (200, 800),
+            2: (358, 711),
+            3: (536, 627),
+            4: (200, 628),
+            5: (371, 544),
+            6: (529, 454),
+            7: (205, 462),
+            8: (361, 391),
+            9: (527, 307)
         }
         return positions.get(key)
 
@@ -61,29 +62,30 @@ class Automator:
         """
         return self._get_position(self.targets.get(target))
 
-    def _match_target(self, target: TargetType):
+    def _match_target(self):
         """
         探测货物，并搬运货物。
         """
-        # 获取当前屏幕快照
-        screen = self.d.screenshot(format="opencv")
+        for x in range(9):
+            for j in range(3):
+                # time.sleep(random.uniform(0.1, 0.3))
+                sx, sy = self.positions_train.get(j + 1)
+                ex, ey = self.positions_location.get(x + 1)
+                self.d.swipe(sx, sy, ex, ey)
 
-        # 由于 OpenCV 的模板匹配有时会智障，故我们探测次数实现冗余。
-        counter = 6
-        while counter != 0:
-            counter = counter - 1
-
-            # 使用 OpenCV 探测货物。
-            result = UIMatcher.match(screen, target)
-
-            # 若无探测到，终止对该货物的探测。
-            # 实现冗余的原因：返回的货物屏幕位置与实际位置存在偏差，导致移动失效
-            if result is None:
-                break
-
-            sx, sy = result
-            # 获取货物目的地的屏幕位置。
-            ex, ey = self._get_target_position(target)
-
-            # 搬运货物。
-            self.d.swipe(sx, sy, ex, ey)
+    positions_location = {
+        1: (200, 790),
+        2: (355, 705),
+        3: (360, 620),
+        4: (200, 620),
+        5: (360, 540),
+        6: (525, 450),
+        7: (200, 460),
+        8: (360, 350),
+        9: (525, 260)
+    }
+    positions_train = {
+        1: (440, 1105),
+        2: (540, 1065),
+        3: (640, 995)
+    }
